e912866e86659bcc6f2e88a7900220dffbe8e5d2
shivamkumar969 <shivamkumar101202@gmail.com>
2026-05-10 21:31:16 +0530

    Fix bulky top space and CSS priority

--- port-folio/src/main.jsx
+++ port-folio/src/main.jsx
@@ -8,31 +8,11 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import "./css/variables.css";
 import "./css/animations.css";
-import "./css/responsive.css";
 import "./App.css";
+import "./css/responsive.css"; // Moved to bottom so it overrides everything else
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
-
-
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-// import "./index.css";
-// import "./css/variables.css";
-// import "./css/animations.css";
-// import "./css/responsive.css";
-// import "./App.css";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
